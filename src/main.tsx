5ccfce9f8d7faf3b9a6260c09ada882763bfb8d3
banderGlitch <nernaykumar98@gmail.com>
2024-08-26 14:30:51 +0530

    metamaskwallet

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { MetaMaskProvider } from "@metamask/sdk-react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <MetaMaskProvider
+      debug={false}
+      sdkOptions={{
+        dappMetadata: {
+          name: "Example React Dapp",
+          url: window.location.href,
+        },
+        // infuraAPIKey: process.env.INFURA_API_KEY,
+        // Other options.
+      }}
+    >
+       <App />
+    </MetaMaskProvider>
+   
   </React.StrictMode>,
 )
